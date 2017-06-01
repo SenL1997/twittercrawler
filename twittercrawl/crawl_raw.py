404e97d54b753a79239cfbf4e3a90b35f9c56f18
Sen <sensjtu@gmail.com>
2017-06-01 23:46:38 +0800

    Update crawl_raw.py

--- twittercrawl/crawl_raw.py
+++ twittercrawl/crawl_raw.py
@@ -6,32 +6,14 @@ Created on 5/30/2017
 '''
 import codecs
 import random
-import sys
 
 import requests
 from bs4 import BeautifulSoup
 
-sys.getdefaultencoding()
-f = codecs.open('t.txt', 'w', encoding='utf-8')
-
-ids = [
-    1390077032,
-    25073877,
-    822215673812119553,
-    2827613550,
-    166862944,
-    781651968729161728,
-    793105171,
-    399811923,
-    464779261,
-    764555761179656192,
-    16526776,
-    2654478201,
-    869415374957101056,
-    1490340577,
-    1350460646,
-    3437553407
-]
+Proxies = {
+    'http':'socks5://127.0.0.1:1080',
+    'https':'socks5://127.0.0.1:1080'
+}
 
 Agent = [
     'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
@@ -40,33 +22,40 @@ Agent = [
     'Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'
 ]
 
-Month = {
-    'Mar': 3,
-    'Aug': 8
-}
 
-
-def id2name(user_id):
+def id2name(user_id, proxies=None):
     '''Convert user_id to user_name'''
     agent = random.choice(Agent)
     headers = {
         'User-Agent': agent
     }
     url = 'https://tweeterid.com/ajax.php'
-    result = requests.post(url, {'input': user_id}, headers=headers)
-    return result.text.split('@')[1]
+    if proxies is not None:
+        result = requests.post(url, {'input': user_id}, headers=headers, proxies=proxies)
+        return result.text.split('@')[1]
+    else:
+        result = requests.post(url, {'input': user_id}, headers=headers)
+        return result.text.split('@')[1]
 
 
-def get_information(user_id):
-    '''Return name, tweet_count, following, follower, favorite, list_count, location, join_date'''
+def get_information(user_inf, flag=0, proxies=None):
+    ''' flag = 0 (default): user_name flag = 1: user_id\n
+        Return name, tweet_count, following, follower, favorite, list_count, location, join_date
+    '''
     agent = random.choice(Agent)
     headers = {
         'User-Agent': agent
     }
     URL = 'https://twitter.com/'
-    name = id2name(user_id)
+    name = user_inf
+    if flag == 1:
+        name = id2name(user_inf,proxies)
     url = URL + name
-    html = BeautifulSoup(requests.get(url, headers=headers).text, 'html5lib')
+    html = 0
+    if proxies is not None:
+        html = BeautifulSoup(requests.get(url, headers=headers, proxies=proxies).text, 'html5lib')
+    else:
+        html = BeautifulSoup(requests.get(url, headers=headers).text, 'html5lib')
     profileNav_list = html.find(
         'ul', {'class': 'ProfileNav-list'}).find_all('li', {'class': 'ProfileNav-item'})
     tweet_count = profileNav_list[0].find(
@@ -89,19 +78,22 @@ def get_information(user_id):
         'div', {'class': 'ProfileHeaderCard'})
     location = profileheader.find('span', {
         'class': 'ProfileHeaderCard-locationText', 'dir': 'ltr'}).text.split('<span')[0].strip()
-    f.write(location + '\r\n')
     join_date = profileheader.find(
         'span', {'class': 'ProfileHeaderCard-joinDateText', 'dir': 'ltr'})['title']
     return name, tweet_count, following, follower, favorite, list_count, location, join_date
 
-def get_status_information(user_name, status_id):
+def get_status_information(user_name, status_id, proxies=None):
     '''Return text, time, reply, retweet, favorite'''
     url = 'https://twitter.com/'+user_name+'/status/'+status_id
     agent = random.choice(Agent)
     headers = {
         'User-Agent':agent
     }
-    html = BeautifulSoup(requests.get(url, headers=headers).text, 'html5lib')
+    html = 0
+    if proxies is not None:
+        html = BeautifulSoup(requests.get(url, headers=headers, proxies=proxies).text, 'html5lib')
+    else:
+        html = BeautifulSoup(requests.get(url, headers=headers).text, 'html5lib')
     text = html.find('div', {'class':'js-tweet-text-container'}).find('p', {'class':'js-tweet-text'}).text
     time = html.find('div', {'class':'js-tweet-details-fixer'}).find('span', {'class':'metadata'}).text.strip()
     tweet_action = html.find('div', {'class':'ProfileTweet-actionCountList'})
@@ -111,10 +103,14 @@ def get_status_information(user_name, status_id):
     return text, time, reply, retweet, favorite
 
 
-def main():
-    for i in ids:
-        print(get_information(i))
+def test():
+    user_id = '25073877'
+    print(get_information(user_id, 1,Proxies))
+    name = 'realDonaldTrump'
+    print(get_information(name,0,Proxies))
+    status = '870234811616677889'
+    print(get_status_information(name, status,Proxies))
 
 
 if __name__ == '__main__':
-    main()
+    test()
