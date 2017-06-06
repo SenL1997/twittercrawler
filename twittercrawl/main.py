2ed8792aa59425159e90ef6e2a3c95d9395e80be
Sen <sensjtu@gmail.com>
2017-06-06 16:37:38 +0800

    Update main.py

--- twittercrawl/main.py
+++ twittercrawl/main.py
@@ -20,11 +20,22 @@ usr_id_list = [
     '2299466747'
 ]
 
+status_id_list = [
+    '869753367299600384',
+    '869753371510546432',
+    '871145660036378624',
+    '870586745468932096',
+    '870709271477932032'
+]
+
 proxies = crawl_raw.Proxies
 
 add_user = ("INSERT INTO user"
             "(name ,id, count,following, follower, favorite, list, location, join_date) "
             "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)") 
+add_status = ("INSERT INTO status"
+              "(status_id, user_id, text, time, reply, retweet, favorite) "
+              "VALUES (%s, %s, %s, %s, %s, %s, %s)")
 
 def craw_user():
     for user_id in usr_id_list:
@@ -38,7 +49,16 @@ def craw_user():
             print(err)
         time.sleep(1)
 
-
+def craw_status():
+    for status_id in status_id_list:
+        try:
+            status_inf = []
+            status_inf = crawl_raw.get_status_information(status_id)
+            data_status = (status_id, status_inf[0], status_inf[1], status_inf[2], status_inf[3], status_inf[4], status_inf[5])
+            cursor.execute(add_status, data_status)
+            cnx.commit()
+        except Exception as err:
+            print(err)
 
 def main():
     craw_user()
